b0014706754394e7649cf2756f00ebc805cc22b1
zelihakaya <66020709+zelihakaya@users.noreply.github.com>
2021-04-30 11:12:40 +0300

    Create food.js

---
+++ food.js
@@ -0,0 +1,28 @@
+import { onSnake, expandSnake } from './snake.js'
+import { randomGridPos } from './grid.js'
+
+let food = getRandomFoodPos()
+const growRate = 1
+
+export function update() {
+  if (onSnake(food)) {
+    expandSnake(growRate)
+    food = getRandomFoodPos()
+  }
+}
+
+export function draw(gameBoard) {
+  const foodElement = document.createElement('div')
+  foodElement.style.gridRowStart = food.y
+  foodElement.style.gridColumnStart = food.x
+  foodElement.classList.add('food')
+  gameBoard.appendChild(foodElement)
+}
+
+function getRandomFoodPos() {
+  let newFoodPos
+  while (newFoodPos == null || onSnake(newFoodPos)) {
+    newFoodPos = randomGridPos()
+  }
+  return newFoodPos
+}
